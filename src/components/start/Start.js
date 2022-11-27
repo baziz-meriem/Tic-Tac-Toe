bb1ea7398df5a9d78138b2e528343f9015ecc6d4
baziz_meriem <mrmbaziz@gmail.com>
2022-11-27 13:32:40 +0100

    start front end done

--- src/components/start/Start.js
+++ src/components/start/Start.js
@@ -1,17 +1,23 @@
 import React from 'react'
+import Xicon from '../icons/Xicon'
+import Oicon from '../icons/Oicon'
+
 
 const Start = () => {
   return (
     <div className="start">
-        <div className="start__header">x o</div>
+        <div className="start__header">
+        <Xicon/>
+        <Oicon/>
+        </div>
         <div className="card shadow-gray">
                 <h1 className="text-lg">Pick player 1'St mark</h1>
                 <div className="start_players">
-                    <span className='start_player'>
-                        x
-                    </span>
                     <span className='start_player--active'>
-                        o
+                        <Xicon color="dark" />
+                    </span>
+                    <span className='start_player'>
+                        <Oicon color="light"/>
                     </span>
                 </div>
                 <p className='text-light'>remember: x goes first</p>
